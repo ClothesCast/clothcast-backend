357e38c43d3d727a6d44cbfb16dcdb8cfe97062e
Sieun Kim <20210760@dongduk.ac.kr>
2025-02-26 15:01:48 +0900

    Initial Commit

---
+++ src/main/java/ktb/clothcast/ClothcastApplication.java
@@ -0,0 +1,13 @@
+package ktb.clothcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothcastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothcastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ktb/clothcast/ClothcastApplicationTests.java
@@ -0,0 +1,13 @@
+package ktb.clothcast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothcastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
